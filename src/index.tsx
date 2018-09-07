899b32a721f76e1579b3d15ebe4b45aea6cf9c2a
DuncanWalter <dwalter@barlingarts.com>
2018-09-07 16:49:21 -0500

    crude boilerplate for configuring tasks

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import Dashboard from './dashboard/Dashboard'
+
 // Atomic react root
 ;(function bootstrap(anchorElement: HTMLElement | null): void {
   if (anchorElement) {
-    render(<div>Hello World!</div>, anchorElement)
+    render(<Dashboard />, anchorElement)
   } else {
     console.error('No anchor element provided')
   }
