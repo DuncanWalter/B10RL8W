cf3c18df62bd3a6b80906b0e26ad64ac878af501
DuncanWalter <dwalter@barlingarts.com>
2018-09-13 18:01:06 -0500

    simulator is working

--- src/simulator/index.ts
+++ src/simulator/index.ts
@@ -1,18 +1,13 @@
 import { range } from '../utils/range'
-import { Card, suits } from './card'
+import { Card, suits, card } from './card'
+import { Player, Quality, createPlayer } from './player'
 
-type Player = {
-  hand: Card[]
-  score: number
-  predictValue(state: State, action: Card): number
-}
-
-type Trick = {
+export type Trick = {
   suit: number | null
   cards: Card[]
 }
 
-type State = {
+export type State = {
   players: [Player, Player, Player, Player]
   trick: Trick
   heartsBroken: boolean
@@ -20,7 +15,7 @@ type State = {
   trickLeader: 0 | 1 | 2 | 3
 }
 
-function trickWinner({ suit, cards }: Trick): Card | null {
+export function trickWinner({ suit, cards }: Trick): Card | null {
   return cards.reduce((winner: null | Card, card: Card) => {
     if (card.suit === suit && (winner === null || card.rank > winner.rank)) {
       return card
@@ -30,8 +25,12 @@ function trickWinner({ suit, cards }: Trick): Card | null {
   }, null)
 }
 
-function validPlays(
-  { trick: { suit }, simplified, heartsBroken }: State,
+export function validPlays(
+  {
+    trick: { suit },
+    simplified,
+    heartsBroken,
+  }: { trick: Trick; simplified: boolean; heartsBroken: boolean },
   hand: Card[],
 ): Card[] {
   if (suit !== null) {
@@ -47,28 +46,32 @@ function validPlays(
   } else if (simplified || heartsBroken) {
     return hand
   } else {
-    return hand.filter(card => {
-      const isHeart = card.suit === suits.hearts
-      const isSpade = card.suit === suits.spades
-      const isQueen = card.rank === 12
-      return !(isHeart || (isQueen && isSpade))
-    })
+    const plays = hand.filter(card => cardPoints(card, simplified) === 0)
+    return plays.length === 0 ? hand : plays
   }
 }
 
-function trickPoints({ trick: { cards }, simplified }: State): number {
-  return cards.reduce((total, card) => {
-    const isHeart = card.suit === suits.hearts
-    const isSpade = card.suit === suits.spades
-    const isQueen = card.rank === 12
-    if (isHeart) {
-      return total + 1
-    } else if (!simplified && isQueen && isSpade) {
-      return total + 13
-    } else {
-      return total
-    }
-  }, 0)
+function cardPoints(card: Card, simplified: boolean): number {
+  const isHeart = card.suit === suits.hearts
+  const isSpade = card.suit === suits.spades
+  const isQueen = card.rank === 12
+  if (isHeart) {
+    return 1
+  } else if (!simplified && isQueen && isSpade) {
+    return 13
+  } else {
+    return 0
+  }
+}
+
+export function trickPoints({
+  trick: { cards },
+  simplified,
+}: {
+  trick: Trick
+  simplified: boolean
+}): number {
+  return cards.reduce((total, card) => total + cardPoints(card, simplified), 0)
 }
 
 function playerWithCard(
@@ -89,61 +92,171 @@ function playerWithCard(
   }
 }
 
-// TODO: how do we want to handle state mutation?
-// TODO: determine whether backprop for any potential methods need input AND error
-function playCard(
-  { trick: { cards } }: State,
-  player: Player,
-  play: Card,
-): void {
-  cards.push(play)
-  player.hand = player.hand.filter(card => card !== play)
+function* freshDeck(): IterableIterator<Card> {
+  for (let suit of Object.keys(suits) as (keyof typeof suits)[]) {
+    for (let rank = 2; rank < 15; rank++) {
+      yield card.create(suit, rank as any)
+    }
+  }
 }
 
-function playRound(state: State) {
-  const {
-    players,
-    trickLeader,
-    trick: { cards },
-  } = state
+function shuffleDeck(deck: Card[], random: () => number = Math.random): void {
+  for (let i = 0; i < deck.length; i++) {
+    const j = (random() * deck.length) | 0
+    const t = deck[i]
+    deck[i] = deck[j]
+    deck[j] = t
+  }
+}
+
+function playCard(
+  state: State,
+  actor: Player,
+  { card, quality }: { card: Card; quality: number },
+): State {
+  const { trick, players } = state
+  const { suit, cards } = trick
+  actor.recordAction(state, card, quality)
+  return {
+    ...state,
+    players: players.map(
+      player =>
+        player === actor
+          ? {
+              ...player,
+              hand: player.hand.filter(handCard => handCard !== card),
+            }
+          : player,
+    ) as [Player, Player, Player, Player],
+    trick: {
+      suit: suit === null ? card.suit : suit,
+      cards: [...cards, card],
+    },
+    heartsBroken:
+      state.heartsBroken || cardPoints(card, state.simplified) !== 0,
+  }
+}
+
+function playRound(startState: State): State {
+  let state = startState
+
   for (let i of range(4)) {
+    const { players, trickLeader } = state
     const player = players[(trickLeader + i) % 4]
     const plays = validPlays(state, player.hand)
-    const play = plays.reduce(
-      (selection, card) => {
-        const value = player.predictValue(state, card)
-        if (selection === null || value > selection.value) {
-          return { value, card, player }
+    const play = player.quality(state, plays).reduce(
+      (selection, { card, quality }) => {
+        if (selection === null || quality > selection.quality) {
+          return { card, quality }
         } else {
           return selection
         }
       },
-      null as null | { value: number; player: Player; card: Card },
+      null as null | { quality: number; card: Card },
     )
     if (play !== null) {
-      playCard(state, player, play.card)
-      // TODO: Do stuff with recording state action qualities
+      const newState = playCard(state, player, play)
+      state = newState
     } else {
       throw new Error('No play was determined')
     }
   }
+
+  const {
+    trick: { cards },
+    players,
+    trickLeader,
+    simplified,
+  } = state
+
   const card = trickWinner(state.trick)
   const points = trickPoints(state)
-  if (card) {
-    const cardIndex = cards.indexOf(card)
-    // TODO: use card location to pick a player
-    // if (player !== undefined) {
-    // } else {
-    //   throw new Error('No trick winner could be determined at round end')
-    // }
-  } else {
+
+  if (card === null) {
     throw new Error('No trick winner could be determined at round end')
+  }
+  const cardIndex = cards.indexOf(card)
+  if (cardIndex === -1) {
+    throw new Error('Trick winning card was not found in trick')
+  }
+  const winningSeat = ((cardIndex + 4 - trickLeader) % 4) as 0 | 1 | 2 | 3
+  const winningPlayer = players[winningSeat]
+  if (winningPlayer === undefined) {
+    throw new Error(
+      'No player could be associated with the card that won the trick',
+    )
+  }
+
+  players.forEach(player => {
+    if (points !== 0 && player === winningPlayer) {
+      player.assignReward(-points)
+    } else {
+      // Assign the bonus of expected points per turn
+      // if the agent collected no points
+      player.assignReward(simplified ? 0.25 : 0.5)
+    }
+  })
+
+  return {
+    ...state,
+    trickLeader: winningSeat,
+    trick: {
+      suit: null,
+      cards: [],
+    },
+    players: players.map(player => {
+      if (points !== 0 && player === winningPlayer) {
+        return {
+          ...player,
+          score: player.score + points,
+        }
+      } else {
+        return player
+      }
+    }) as [Player, Player, Player, Player],
   }
 }
 
-// function playGame() {
-//   let i = 0
-//   while (i++ < 13) {
-//     playRound()
-//   }
-// }
+export function playGame(
+  policies: [Quality, Quality, Quality, Quality],
+  simplified: boolean,
+  random: () => number = Math.random,
+) {
+  const deck = [...freshDeck()]
+  shuffleDeck(deck, random)
+
+  const players: Player[] = policies.map((policy, seat) =>
+    createPlayer(policy, deck.slice(seat * 13, (1 + seat) * 13)),
+  )
+
+  const startingPlayer = playerWithCard(players, 'clubs', 2)
+  if (startingPlayer === null) {
+    throw new Error(
+      'The two of clubs was not dealt, so no start player can be determined',
+    )
+  }
+
+  const startingSeat = players.indexOf(startingPlayer) as -1 | 0 | 1 | 2 | 3
+  if (startingSeat === -1) {
+    throw new Error(
+      'No player matched the one found to have the two of clubs, so no start player could be determined ',
+    )
+  }
+
+  let state: State = {
+    players: players as [Player, Player, Player, Player],
+    trick: { suit: null, cards: [] },
+    simplified,
+    heartsBroken: false,
+    trickLeader: startingSeat,
+  }
+
+  for (let i of range(13)) {
+    state = playRound(state)
+  }
+
+  return players.map(player => {
+    player.assignReward(((simplified ? 3.25 : 6.5) - player.score) * 2)
+    return player.terminate()
+  })
+}
---
+++ src/simulator/simulator.test.ts
@@ -0,0 +1,120 @@
+import { suits, card, Card } from './card'
+import {
+  trickPoints,
+  Trick,
+  validPlays,
+  trickWinner,
+  playGame,
+  State,
+} from './index'
+
+test('Tricks are scored correctly', () => {
+  const trick: Trick = {
+    suit: null,
+    cards: [card.create('clubs', 3), card.create('diamonds', 12)],
+  }
+
+  expect(trickPoints({ trick, simplified: true })).toEqual(0)
+  expect(trickPoints({ trick, simplified: false })).toEqual(0)
+
+  trick.cards.push(card.create('hearts', 5), card.create('spades', 12))
+
+  expect(trickPoints({ trick, simplified: true })).toEqual(1)
+  expect(trickPoints({ trick, simplified: false })).toEqual(14)
+})
+
+test('Trick taking cards are correctly determined', () => {
+  let cards = [
+    card.create('clubs', 3),
+    card.create('clubs', 12),
+    card.create('hearts', 5),
+    card.create('spades', 12),
+  ]
+  expect(trickWinner({ suit: suits.clubs, cards })).toBe(cards[1])
+  cards[0] = card.create('diamonds', 2)
+  expect(trickWinner({ suit: suits.diamonds, cards })).toBe(cards[0])
+  cards[0] = card.create('spades', 14)
+  expect(trickWinner({ suit: suits.spades, cards })).toBe(cards[0])
+})
+
+test('Legal moves are correctly identified', () => {
+  const hand = [
+    card.create('clubs', 3),
+    card.create('clubs', 12),
+    card.create('hearts', 5),
+    card.create('spades', 12),
+  ]
+  // lead off in simplified game
+  expect(
+    validPlays(
+      {
+        trick: { suit: null, cards: [] },
+        simplified: true,
+        heartsBroken: false,
+      },
+      hand,
+    ).length,
+  ).toEqual(4)
+  // lead off with hearts broken
+  expect(
+    validPlays(
+      {
+        trick: { suit: null, cards: [] },
+        simplified: false,
+        heartsBroken: true,
+      },
+      hand,
+    ).length,
+  ).toEqual(4)
+  // lead off restricted
+  expect(
+    validPlays(
+      {
+        trick: { suit: null, cards: [] },
+        simplified: false,
+        heartsBroken: false,
+      },
+      hand,
+    ).length,
+  ).toEqual(2)
+  // forced to play a card
+  expect(
+    validPlays(
+      {
+        trick: { suit: suits.hearts, cards: [] },
+        simplified: false,
+        heartsBroken: true,
+      },
+      hand,
+    ).length,
+  ).toEqual(1)
+  // forced to play a suit
+  expect(
+    validPlays(
+      {
+        trick: { suit: suits.clubs, cards: [] },
+        simplified: true,
+        heartsBroken: false,
+      },
+      hand,
+    ).length,
+  ).toEqual(2)
+  // free to set
+  expect(
+    validPlays(
+      {
+        trick: { suit: suits.diamonds, cards: [] },
+        simplified: false,
+        heartsBroken: false,
+      },
+      hand,
+    ).length,
+  ).toEqual(4)
+})
+
+test('Plays a game and produces agent histories', () => {
+  const policy = (state: State, actions: Card[]) =>
+    actions.map(action => ({ card: action, quality: Math.random() }))
+
+  const history = playGame([policy, policy, policy, policy], false)
+})
