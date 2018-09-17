56f3f557ea17e3c471659854983bff8cad925745
G. Eli Jergensen <gelijergensen@ou.edu>
2018-09-16 20:09:25 -0500

    Added logging hook in training

--- src/agents/training.ts
+++ src/agents/training.ts
@@ -2,10 +2,26 @@ import { Agent, interpretHistory, FeedBack } from '.'
 import { playGame } from '../simulator'
 import { range } from '../utils/range'
 
-export function trainAgent<F>({ policy, train }: Agent<F>, games: number) {
+export function trainAgent<F, S>(
+  { policy, train, summary }: Agent<F, S>,
+  games: number,
+  sessionName: string,
+  log: (
+    simplified: boolean,
+    suitCount: number,
+    sessionName: string,
+    additionalGamesPlayed: number,
+    agentSummary: S,
+  ) => void,
+) {
+  const simplified = false
+  const suitCount = 4
+  let lastGameRecorded = 0
+  let timerStart = Date.now()
+  let timerEnd: number
   for (let i of range(games)) {
     train(
-      playGame([policy, policy, policy, policy], false)
+      playGame([policy, policy, policy, policy], simplified)
         .map(interpretHistory)
         .reduce(
           (acc, { feedBack }) => {
@@ -15,5 +31,22 @@ export function trainAgent<F>({ policy, train }: Agent<F>, games: number) {
           [] as FeedBack<F>[],
         ),
     )
+    if (i % 100 === 0) {
+      timerEnd = Date.now()
+      if (timerEnd - timerStart > 10000) {
+        const additionalGamesPlayed = i - lastGameRecorded
+        const agentSummary = summary()
+        // TODO Implement log function in dashboard
+        log(
+          simplified,
+          suitCount,
+          sessionName,
+          additionalGamesPlayed,
+          agentSummary,
+        )
+        lastGameRecorded = i
+        timerStart = timerEnd
+      }
+    }
   }
 }
