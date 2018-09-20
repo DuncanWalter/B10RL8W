de0cd2cbeb817a28c730396c101325f06ee5d4b3
DuncanWalter <dwalter@barlingarts.com>
2018-09-20 17:34:34 -0500

    my changes to our NN sources that seem to improve learning rate and stability

--- src/neural-net/split_vanilla_ann.test.ts
+++ src/neural-net/split_vanilla_ann.test.ts
@@ -13,10 +13,10 @@ test('The xor function works', () => {
   let xData = [[0, 0], [0, 1], [1, 0], [1, 1]]
   let yData = [[0], [1], [1], [-1]]
 
-  let ann = new Split_Vanilla_ANN(0.01, [
+  let ann = new Split_Vanilla_ANN(0.12, [
     { nodes: 2 },
-    { nodes: 12, activation: signActivation },
-    { nodes: 12, activation: signActivation },
+    { nodes: 7, activation: signActivation },
+    { nodes: 3, activation: signActivation },
     { nodes: 1, activation: { feed: i => i, prime: i => 1 } },
   ])
   for (let epoch of range(3000)) {
@@ -24,6 +24,9 @@ test('The xor function works', () => {
     for (let i in xData) {
       const { output, feedTrace } = ann.feed(xData[i])
       const error = [yData[i][0] - output[0]]
+      if (epoch % 300 === 0) {
+        console.log(error[0])
+      }
       feedBack.push({ feedTrace, error: error })
     }
     ann.backProp(feedBack)
