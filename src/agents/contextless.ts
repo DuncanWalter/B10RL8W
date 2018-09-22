f8fed5d840af46a04341605b408072ee6f3df672
Duncan Walter <dwalter@barlingarts.com>
2018-09-22 16:59:22 -0500

    Machinery for translating games into NN inputs (#33)

--- src/agents/contextless.ts
+++ src/agents/contextless.ts
@@ -1,12 +1,10 @@
 import {
   Player,
   State,
-  Trick,
   cardPoints,
   trickWinner,
-  trickPoints,
   Card,
-  card,
+  suits,
 } from '../simulator'
 import { Agent, FeedBack } from '.'
 
@@ -26,51 +24,90 @@ export type ANNSummary = {
   qualityWeights: number[][][]
 }
 
-function* joinIterables(
-  a: Iterable<number>,
-  b: Iterable<number>,
-): IterableIterator<number> {
-  yield* a
-  yield* b
+export type GameSummary<L extends number> = {
+  size: L
+  summary(state: State, player: Player, action: Card): Iterable<number>
 }
 
-// TODO: could also count the number of each suit in hand
-function handData(hand: Card[], simplified: boolean): Iterable<number> {
-  return hand
-    .map(card => ({
-      card,
-      score: card.rank + (cardPoints(card, simplified) === 0 ? 0 : 5),
-    }))
-    .sort((a, b) => a.score - b.score)
-    .slice(0, 5)
-    .map(({ card: obj }) => card.data(obj))
-    .reduce(joinIterables, [hand.length])
+const handSummary: GameSummary<14> = {
+  size: 14,
+  summary(state: State, { hand: rawHand }: Player, action: Card) {
+    const hand = rawHand.filter(card => card !== action)
+    const pips = hand.reduce((pips, card) => pips + card.rank, 0)
+    const cards = hand.length
+    const [hearts, spades, clubs, diamonds] = [0, 1, 2, 3].map(suit => {
+      let count = 0
+      let min = 14
+      let max = 2
+      hand.forEach(card => {
+        if (card.suit === suit) {
+          count++
+          max = Math.max(max, card.rank)
+          min = Math.min(min, card.rank)
+        }
+      })
+      return [count, min, max]
+    })
+    return [pips, cards, ...hearts, ...spades, ...clubs, ...diamonds]
+  },
 }
 
-function trickData(
-  trick: Trick,
-  action: Card,
-  simplified: boolean,
-): Iterable<number> {
-  return [
-    // whether this action would take the trick
-    trickWinner({ cards: [...trick.cards, action], suit: trick.suit }) ===
-    action
-      ? 1
-      : 0,
-    trick.cards.length,
-    trickPoints(trick, simplified),
-  ]
+const trickSummary: GameSummary<4> = {
+  size: 4,
+  summary({ simplified, trick }: State, player: Player, action: Card) {
+    const trickWithAction = [action, ...trick.cards]
+    const points = trickWithAction.reduce(
+      (points, card) => points + cardPoints(card, simplified),
+      0,
+    )
+    const cards = trick.cards.length
+    const takesTrick =
+      trickWinner({ cards: trickWithAction, suit: trick.suit }) === action
+        ? 1
+        : 0
+    const trickRank = (trickWinner(trick) || { rank: 0 }).rank
+    return [points, cards, takesTrick, trickRank]
+  },
 }
+
+const actionSummary: GameSummary<5> = {
+  size: 5,
+  summary(state: State, player: Player, action: Card) {
+    return [
+      ...(Object.keys(suits) as (keyof typeof suits)[]).map(suit => {
+        return action.suit === suits[suit] ? 1 : 0
+      }),
+      action.rank,
+    ]
+  },
+}
+
+export function joinSummaries(
+  ...summaries: GameSummary<number>[]
+): GameSummary<number> {
+  return {
+    size: summaries.reduce((totalSize, { size }) => size + totalSize, 0),
+    *summary(state: State, player: Player, action: Card) {
+      for (let { summary } of summaries) {
+        yield* summary(state, player, action)
+      }
+    },
+  }
+}
+
+export const contextlessSummary = joinSummaries(
+  handSummary,
+  trickSummary,
+  actionSummary,
+)
 
 export function createContextlessAgent(
   net: ANN,
 ): Agent<number[][], ANNSummary> {
   return {
-    policy({ trick, simplified }: State, player: Player, actions: Card[]) {
-      const hand = [...handData(player.hand, simplified)]
+    policy(state: State, player: Player, actions: Card[]) {
       return actions
-        .map(action => [...hand, ...trickData(trick, action, simplified)])
+        .map(action => [...contextlessSummary.summary(state, player, action)])
         .map(data => net.feed(data))
         .map(({ feedTrace, output: [quality] }, index) => ({
           quality,
