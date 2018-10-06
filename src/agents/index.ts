a6b9cbb20cb8acc79482edd0c287777308621451
Duncan Walter <dwalter@barlingarts.com>
2018-10-05 20:22:26 -0500

    some docs (#55)

--- src/agents/index.ts
+++ src/agents/index.ts
@@ -1,6 +1,14 @@
 import { Policy } from '../simulator/player'
 import { FeedBack } from './history'
 
+/**
+ * Agents are a behavior within the environment / simulation
+ * which are capable of expressing opinions on taking different actions.
+ * Agents can also be given feedback (the results of their actions) which
+ * the agent can use however it sees fit under the hood. Finally, agents can
+ * be serialized for saving and loading purposes.
+ */
+
 export type Agent<F = unknown, T = string> = {
   policy: Policy<F>
   train: (
--- src/simulator/index.ts
+++ src/simulator/index.ts
@@ -1,3 +1,15 @@
+/**
+ * Simulating Hearts is pretty straightforward. We've taken an immutable
+ * state approach which allows us to cache game states for later without
+ * fear of trampling our data. The more interesting part of our simulator is
+ * the history collection. The game records all actions made by all players
+ * along with the player's policy's (policies are taken from agent objects)
+ * scratch work for choosing that action. This allows us to run the backwards
+ * pass on large batches while only running the forward pass on decision
+ * instances. This cuts our training time by at least a third if forward and
+ * backwards passes are similarly difficult to compute.
+ */
+
 export { Card, suits, card } from './card'
 export { Player, History, Policy, ActionSummary, createPlayer } from './player'
 export {
