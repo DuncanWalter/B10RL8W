54bd3b1a244ae6372df076d87a4ae66c80797cb0
DuncanWalter <dwalter@barlingarts.com>
2018-10-09 13:28:11 -0500

    making tests not fall through

--- src/agents/training.test.ts
+++ src/agents/training.test.ts
@@ -12,13 +12,19 @@ import { trainAgent } from './training'
   cardCountingSummary,
   cardSharkSummary,
 ].forEach(summary => {
-  test(`Running ${summary} agents works`, () => {
-    trainAgent(createAgent(summary), 2, true, (epoch, mean, stdDev) => {
-      if (epoch % 40 === 39) {
-        // console.log(
-        //   `epoch ${epoch + 1}: \tmean ${mean | 0} \tstdDev ${stdDev | 0}`,
-        // )
-      }
-    })
+  test(`Running ${summary} agents works`, done => {
+    trainAgent(
+      createAgent(summary),
+      2,
+      true,
+      (epoch, mean, stdDev) => {
+        if (epoch % 40 === 39) {
+          // console.log(
+          //   `epoch ${epoch + 1}: \tmean ${mean | 0} \tstdDev ${stdDev | 0}`,
+          // )
+        }
+      },
+      done,
+    )
   })
 })
