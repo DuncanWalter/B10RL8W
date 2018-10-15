48087fd674181040e47439b01e2f5f758530f75a
G. Eli Jergensen <gelijergensen@ou.edu>
2018-10-14 19:08:37 -0500

    Enabled the cancel button while training (#75)

--- src/dashboard/TrainDialog.tsx
+++ src/dashboard/TrainDialog.tsx
@@ -3,7 +3,7 @@ import { trainAgent } from '../web-worker/service'
 import Card from './Card'
 import CardHeader from './CardHeader'
 import CardContent from './CardContent'
-import Typography from '@material-ui/core/Typography'
+import LinearProgress from '@material-ui/core/LinearProgress'
 import Button from './Button'
 import { TrainingProgressMessage } from '../web-worker/protocol'
 import SplinePlot from './SplinePlot'
@@ -13,6 +13,8 @@ export type TrainDialogProps = any
 export type TrainDialogState = {
   epochAndYMetric: { x: number; y: number; yDev: number }[]
   doneTraining: boolean
+  isTraining: boolean
+  cancelFn: () => void
 }
 
 export default class TrainDialog extends React.Component<
@@ -24,6 +26,8 @@ export default class TrainDialog extends React.Component<
     this.state = {
       epochAndYMetric: [],
       doneTraining: false,
+      isTraining: false,
+      cancelFn: () => { },
     }
   }
 
@@ -32,12 +36,22 @@ export default class TrainDialog extends React.Component<
     const agentType = 'guru'
     const epochs = 9000
     const onProgress = this.trainingCallback
-    const simplified = false
-    trainAgent({ agentName, agentType, epochs, onProgress, simplified }).then(
-      () => {
-        this.setState({ doneTraining: true })
-      },
-    )
+    const simplified = true
+    const promise = trainAgent({
+      agentName,
+      agentType,
+      epochs,
+      onProgress,
+      simplified,
+    })
+    promise.then(() => {
+      this.setState({
+        doneTraining: true,
+        isTraining: false,
+        cancelFn: () => { },
+      })
+    })
+    this.setState({ cancelFn: promise.cancel, isTraining: true })
   }
 
   trainingCallback = (snapshot: TrainingProgressMessage) => {
@@ -52,8 +66,18 @@ export default class TrainDialog extends React.Component<
     })
   }
 
+  stopTraining = () => {
+    const { cancelFn } = this.state
+    cancelFn()
+    this.setState({
+      isTraining: false,
+      doneTraining: true,
+      cancelFn: () => { },
+    })
+  }
+
   render() {
-    const { epochAndYMetric, doneTraining } = this.state
+    const { epochAndYMetric, doneTraining, isTraining } = this.state
     const data = [
       {
         id: 'line',
@@ -75,14 +99,25 @@ export default class TrainDialog extends React.Component<
         color: '#999999',
       },
     ]
-    const doneMessage = doneTraining ? (
+    const activityIndicator = isTraining ? (
       <CardContent>
-        <Typography variant="body1">We have completed training!</Typography>
+        <LinearProgress />
       </CardContent>
     ) : (
         undefined
       )
-
+    const rightButton = doneTraining ? (
+      undefined
+    ) : isTraining ? (
+      <Button
+        text="Cancel"
+        onClick={this.stopTraining}
+        disabled={epochAndYMetric.length <= 1}
+        variant="danger"
+      />
+    ) : (
+          <Button text="Train Agent" onClick={this.callTrainAgent} />
+        )
     return (
       <Card>
         <CardHeader>Train New Agent</CardHeader>
@@ -100,10 +135,10 @@ export default class TrainDialog extends React.Component<
             yAxisTitle={'average performance'}
           />
         </CardContent>
-        {doneMessage}
+        {activityIndicator}
         <CardContent style={{ display: 'flex', justifyContent: 'flex-end' }}>
           <Button text="Dismiss" />
-          <Button text="Train Agent" onClick={this.callTrainAgent} />
+          {rightButton}
         </CardContent>
       </Card>
     )
